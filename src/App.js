c6e94b1e95908bb70737c811eab14322352ae4e9
Anushk Jain <anushk2201@gmail.com>
2024-01-10 23:15:46 +0530

    feat: added event handlers.

--- src/App.js
+++ src/App.js
@@ -6,17 +6,37 @@ class Counter extends React.Component {
     super(props);
 
     // One huge state object, not the multiple state variables like we do with the use state hook
-    this.state = { count: 5 }
+    this.state = { count: 5 };
+
+    // Inside the JSX, the handlers function loses the value of this keyword that's why we have to bind this keyword to the handlers function like this
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
 
   }
 
+  handleDecrement() {
+    this.setState((currState) => {
+      return {count: currState.count - 1};
+    });
+  }
+
+  handleIncrement() {
+    this.setState((currState) => {
+      return {count: currState.count + 1};
+    });
+  }
+
   render() {
+    const date = new Date('june 21 2022');
+
+    date.setDate(date.getDate() + this.state.count);
+
     return <div>
-      <button>-</button>
+      <button onClick={this.handleDecrement}>-</button>
 
-      <span>{this.state.count}</span>
+      <span>{date.toDateString()} [{this.state.count}]</span>
 
-      <button>+</button>
+      <button onClick={this.handleIncrement}>+</button>
     </div>
   }
 }
