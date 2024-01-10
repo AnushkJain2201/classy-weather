e293129841c677b9b7d1a8f8cb20d145dcbf0b11
Anushk Jain <anushk2201@gmail.com>
2024-01-10 22:38:54 +0530

    feat: created our first class component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-function App() {
-  return (
-    <div>
-      
+import React from "react";
+
+class Counter extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    // One huge state object, not the multiple state variables like we do with the use state hook
+    this.state = { count: 5 }
+
+  }
+
+  render() {
+    return <div>
+      <button>-</button>
+
+      <span>{this.state.count}</span>
+
+      <button>+</button>
     </div>
-  );
+  }
 }
 
-export default App;
+export default Counter;
