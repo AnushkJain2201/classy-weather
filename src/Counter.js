405d93b001966d8eb442d18bcb68cb495e1ef84e
Anushk Jain <anushk2201@gmail.com>
2024-01-11 20:24:40 +0530

    feat: fetched weather data.

--- src/Counter.js
+++ src/Counter.js
@@ -1,44 +1,46 @@
 import React from "react";
 
 class Counter extends React.Component {
+	constructor(props) {
+		super(props);
 
-  constructor(props) {
-    super(props);
+		// One huge state object, not the multiple state variables like we do with the use state hook
+		this.state = { count: 5 };
 
-    // One huge state object, not the multiple state variables like we do with the use state hook
-    this.state = { count: 5 };
+		// Inside the JSX, the handlers function loses the value of this keyword that's why we have to bind this keyword to the handlers function like this
+		this.handleDecrement = this.handleDecrement.bind(this);
+		this.handleIncrement = this.handleIncrement.bind(this);
+	}
 
-    // Inside the JSX, the handlers function loses the value of this keyword that's why we have to bind this keyword to the handlers function like this
-    this.handleDecrement = this.handleDecrement.bind(this);
-    this.handleIncrement = this.handleIncrement.bind(this);
+	handleDecrement() {
+		this.setState((currState) => {
+			return { count: currState.count - 1 };
+		});
+	}
 
-  }
+	handleIncrement() {
+		this.setState((currState) => {
+			return { count: currState.count + 1 };
+		});
+	}
 
-  handleDecrement() {
-    this.setState((currState) => {
-      return {count: currState.count - 1};
-    });
-  }
+	render() {
+		const date = new Date("june 21 2022");
 
-  handleIncrement() {
-    this.setState((currState) => {
-      return {count: currState.count + 1};
-    });
-  }
+		date.setDate(date.getDate() + this.state.count);
 
-  render() {
-    const date = new Date('june 21 2022');
+		return (
+			<div>
+				<button onClick={this.handleDecrement}>-</button>
 
-    date.setDate(date.getDate() + this.state.count);
+				<span>
+					{date.toDateString()} [{this.state.count}]
+				</span>
 
-    return <div>
-      <button onClick={this.handleDecrement}>-</button>
-
-      <span>{date.toDateString()} [{this.state.count}]</span>
-
-      <button onClick={this.handleIncrement}>+</button>
-    </div>
-  }
+				<button onClick={this.handleIncrement}>+</button>
+			</div>
+		);
+	}
 }
 
 export default Counter;
